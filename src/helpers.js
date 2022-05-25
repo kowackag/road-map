da81175ce5697d1cda7c979ffc15d4f450e4bfec
Kowacka Malgorzata <kowackag@gmail.com>
2022-05-25 21:14:59 +0200

    Add second version to calculate price

--- src/helpers.js
+++ src/helpers.js
@@ -1,21 +1,39 @@
+// -----------version 1-------------------
+
 export const calculator = (dist, price) => {
   const limitDist = 800;
-  const limitPrice = 1000;
+  const pricePerDay = 1000;
 
-  const countTotalPrice = (dist, price) => {
-    return (Number(price) * Number(dist) * 1.1).toFixed(2);
-  };
-  const totalPrice = countTotalPrice(dist, price);
+  const totalTime = Math.ceil(Number(dist) / limitDist);
 
-  const timeCountedByDistance = Math.ceil(Number(dist) / limitDist);
-  const timeCountedByPrice = Math.ceil(
-    (Number(price) * Number(dist)).toFixed(2) / limitPrice
-  );
-
-  const totalTime =
-    timeCountedByDistance > timeCountedByPrice
-      ? timeCountedByDistance
-      : timeCountedByPrice;
+  const totalPrice = (
+    (Number(price) * Number(dist) + totalTime * pricePerDay) *
+    1.1
+  ).toFixed(2);
 
   return { totalPrice, totalTime };
 };
+
+// -----------version 2-------------------
+
+// export const calculator = (dist, price) => {
+//   const limitDist = 800;
+//   const limitPrice = 1000;
+
+//   const countTotalPrice = (dist, price) => {
+//     return (Number(price) * Number(dist) * 1.1).toFixed(2);
+//   };
+//   const totalPrice = countTotalPrice(dist, price);
+
+//   const timeCountedByDistance = Math.ceil(Number(dist) / limitDist);
+//   const timeCountedByPrice = Math.ceil(
+//     (Number(price) * Number(dist)).toFixed(2) / limitPrice
+//   );
+
+//   const totalTime =
+//     timeCountedByDistance > timeCountedByPrice
+//       ? timeCountedByDistance
+//       : timeCountedByPrice;
+
+//   return { totalPrice, totalTime };
+// };
